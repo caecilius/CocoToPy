8b94116c10c50a07b194f11c9d05779829ff59ae
caecilius <caecilius@firemail.cc>
2016-05-22 19:34:07 +0200

    Update API.py

--- API.py
+++ API.py
@@ -71,4 +71,73 @@ serveur_pass = # ?
 
 url3 = str("35519201080" + "*" + "0" + "*" + "0" + "*" + "0" + "*" + "192.168.1.1" + "*" + IPlocale + serveur_pass)
 
+n = str(35519201080*0*0*0*192.168.1.1*" + IPlocale)
+y = serveur_pass
+z = 0
+
+"""
+à convertir en Python 
+
+function enxo(n,y,z) {
+    
+    var o = "";
+    var chr1, chr2, chr3 = "";
+    var enc=[];
+    var revo=[];
+    for (i=0; i<65; i++)
+    revo[doc[i]]=i;
+    var i = 0;
+    if(z==1)
+    {
+    do {
+        for(j=0;j<4;j++)
+        enc[j]=revo[n.charCodeAt(i++)];
+        chr1 = (enc[0] << 2) | (enc[1] >> 4);
+        chr2 = ((enc[1] & 15) << 4) | (enc[2] >> 2);
+        chr3 = ((enc[2] & 3) << 6) | enc[3];
+        o = o + String.fromCharCode(chr1);
+        if (enc[2] != 64)
+        o = o + String.fromCharCode(chr2);
+        if (enc[3] != 64)
+        o = o + String.fromCharCode(chr3);
+        
+    } while (i < n.length);
+    n=o;    
+    }
+    var result="";
+for(i=0;i<n.length;++i)
+result+=String.fromCharCode(y.charCodeAt(i % y.length)^n.charCodeAt(i));
+
+if(z==1)
+o=result;
+i=0;    
+
+    if(z==0)
+    {       
+    n=result;
+    
+    do {
+        chr1 = n.charCodeAt(i++);
+        chr2 = n.charCodeAt(i++);
+        chr3 = n.charCodeAt(i++);
+
+        enc[0] = chr1 >> 2;
+        enc[1] = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc[2] = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc[3] = chr3 & 63;
+        if (isNaN(chr2)) {
+            enc[2] = enc[3] = 64;
+        } else if (isNaN(chr3)) {
+            enc[3] = 64;
+        }
+
+        for(j=0;j<4;j++)
+        o += String.fromCharCode(doc[enc[j]]);
+    } while (i < n.length);
+    }
+    
+    return o;
+}
+"""
+
 r = requests.get(url3)
