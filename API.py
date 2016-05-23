1135a9e2614399db733b6a6f2448a30298df0e01
caecilius <caecilius@firemail.cc>
2016-05-23 08:21:51 +0200

    Update API.py

--- API.py
+++ API.py
@@ -9,36 +9,32 @@ import random
 import requests
 import re
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Identification
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Génération des valeurs aléatoires
 
-Salt   = str(random.randrange(19939050, 99939050))
-Salt2  = str(random.randrange(19939050, 99939050))
-Avatar = str(random.randrange(164249067, 964249067))
+salt   = str(random.randrange(19939050, 99939050))
+salt2  = str(random.randrange(19939050, 99939050))
+avatar = str(random.randrange(164249067, 964249067))
 
 # Atribution des variables utilisateur
 
-Pseudo      = "Caecilius"  # Minimum 4 caractères
-Age         = "22"         # Minimum 18
-Sexe        = "1"          # 1 pour homme et 2 pour femme
-CodePostal  = "75005"      # Uniquement en France
+pseudo       = "Caecilius"  # Minimum 4 caractères
+age          = "18"         # Minimum 18
+sexe         = "1"          # 1 pour homme et 2 pour femme
+code_postal  = "69005"      # Uniquement en France
 
-# Récupération du CodeVille
+# Récupération du code_ville
 
-r = requests.get(str("http://coco.fr/cocoland/" + CodePostal + ".js"))
-CodeVille = re.search(r'(\d+)', r.text).group(1)
+r = requests.get(str("http://coco.fr/cocoland/" + code_postal + ".js"))
+code_ville = re.search(r'(\d+)', r.text).group(1)
 
 # Génération de l'URL
 
-URL = str("http://87.98.162.169/40" \
-+  Pseudo + "*" + Age + Sexe + CodeVille + Avatar + "0?" + Salt + "." + Salt2)
+serveur_url = str("http://87.98.162.169/40" \
++  pseudo + "*" + age + sexe + codeVille + avatar + "0?" + salt + "." + salt2)
 
 # Envoie de la requête GET
 
-r = requests.get(URL)
+r = requests.get(serveur_url)
 
 # Récupération des variables d'identification avec des regex
 
@@ -51,15 +47,7 @@ r = requests.get(r'http://jsonip.com')
 IpExterne = r.json()['ip']
 
 # TODO
-# Fonction pour chiffrer l'URL (https://bpaste.net/show/9744211f8e4e)
+# Fonction pour chiffrer l'URL (https://bpaste.net/show/f5afa3a02692)
 # Génération de l'URL chiffrée
 # Envoie de la requête GET
 # Formatage de la réponse
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Liste des utilisateurs
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Discussion privée
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
